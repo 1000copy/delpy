6c38b2646fc8bd0ba6309d591e0f175f7e6f909f
1000copy <1000copy@fb7a4105-3035-0410-82e1-b15e11f8ce98>
2007-10-11 14:24:32 +0000

    //清清月儿http://blog.csdn.net/21aspnet
    //2007-10-11
    //1000copy http://1000copy.spaces.live.com 改编

---
+++ trunk/src/GridViewTutorial/CRUD/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" EnableSessionState="True" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>无标题页</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4"
+                        ForeColor="#333333" GridLines="None" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing"
+                        OnRowUpdating="GridView1_RowUpdating" OnRowCancelingEdit="GridView1_RowCancelingEdit">
+                        <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                        <Columns>
+                            <asp:BoundField DataField="Id" HeaderText="用户ID" ReadOnly="True" />
+                            <asp:BoundField DataField="Name" HeaderText="用户姓名" />
+                            <asp:TemplateField HeaderText="级别">
+                                <EditItemTemplate>
+                                    <asp:Label ID="Label1" runat="server" Text='<%# Eval("Level") %>'></asp:Label>
+                                    <asp:DropDownList ID="DropDownList1" runat="server" DataSource='<%# LevelList()%>' 
+                                    DataValueField="Id" DataTextField="Name"/>
+                                </EditItemTemplate>
+                                <ItemTemplate>
+                                    <asp:Label ID="Label1" runat="server" Text='<%# Bind("Level") %>'></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:BoundField DataField="Id" HeaderText="家庭住址" ReadOnly="True"/>
+                            <asp:CommandField HeaderText="选择" ShowSelectButton="True" />
+                            <asp:CommandField HeaderText="编辑" ShowEditButton="True" />
+                            <asp:CommandField HeaderText="删除" ShowDeleteButton="True" />
+                        </Columns>
+                        <RowStyle ForeColor="#000066" />
+                        <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                        <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                        <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                    </asp:GridView>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ trunk/src/GridViewTutorial/CRUD/Default.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+using System.Collections;
+public partial class _Default : System.Web.UI.Page
+{
+
+    //清清月儿http://blog.csdn.net/21aspnet 
+    // 1000copy http://1000copy.spaces.live.com 改编 2007-10-11
+    ArrayList personarray = null;
+    public _Default()
+    {
+        //if (Session["PersonArray"] == null)
+        //{
+
+            personarray = new ArrayList();
+            Hero person = new Hero();
+            person.Id = "11";
+            person.Name = "宋江";
+            person.Level = "1";
+            personarray.Add(person);
+            person = new Hero();
+            person.Id = "12";
+            person.Level = "2";
+            person.Name = "李逵";
+            personarray.Add(person);
+            person = new Hero();
+            person.Id = "13";
+            person.Level = "2";
+            person.Name = "花荣";
+            personarray.Add(person);
+            person = new Hero();
+            person.Id = "14";
+            person.Level = "2";
+            person.Name = "武松";
+            personarray.Add(person);
+            //Session["PersonArray"] = personarray;
+        //}
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!IsPostBack)
+        {
+          
+            bind();
+        }
+    }
+    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        bind();
+    }
+
+    //删除
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        string currentkey = GridView1.DataKeys[e.RowIndex].Value.ToString();
+        //personarray = (ArrayList)Session["PersonArray"];
+        foreach (Hero person in personarray)
+        {
+            if (currentkey == person.Id)
+            {
+                personarray.Remove(person);
+                break;
+            }
+        } 
+        bind();
+    }
+
+    //更新
+    protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        string newname = ((TextBox)(GridView1.Rows[e.RowIndex].Cells[1].Controls[0])).Text.ToString().Trim();
+        string newlevel = ((DropDownList)(GridView1.Rows[e.RowIndex].Cells[2].FindControl("DropDownList1"))).SelectedValue.ToString().Trim();
+        string currentkey = GridView1.DataKeys[e.RowIndex].Value.ToString();
+        //personarray = (ArrayList)Session["PersonArray"];
+        foreach (Hero person in personarray)
+        {
+            if (currentkey == person.Id)
+            {
+                person.Name = newname;
+                person.Level = newlevel;
+            }
+        }
+        GridView1.EditIndex = -1;
+        bind();
+    }
+
+    //取消
+    protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        bind();
+    }
+
+    //绑定
+    public void bind()
+    {
+      //  personarray = (ArrayList)Session["PersonArray"];
+      GridView1.DataSource = personarray;
+      GridView1.DataKeyNames = new string[] { "id" };//主键
+      GridView1.DataBind();
+      for (int i = 0; i <= GridView1.Rows.Count - 1; i++)
+      {          
+          DropDownList ddl = (DropDownList)GridView1.Rows[i].FindControl("DropDownList1");
+          if (ddl != null)
+          {
+              if ("13" == GridView1.DataKeys[i].Value.ToString())
+              {
+                  ddl.SelectedValue = "1";
+              }
+              else
+              {
+                  ddl.SelectedValue = "2";
+              }
+          }
+      }
+
+    }
+    public ArrayList LevelList()
+    {
+        ArrayList al = new ArrayList();
+        HeroLevel a = new HeroLevel();
+        a.Name = "天罡星";
+        a.Id ="1";        
+        al.Add(a);
+        a = new HeroLevel();
+        a.Name = "地煞星";
+        a.Id = "2";
+        al.Add(a);
+        return al;
+    }
+
+}
+public class Hero
+{
+    private string _name = "";
+    private string _id = "";
+    private string _level = "";
+
+    public string Level
+    {
+        get { return _level; }
+        set { _level = value; }
+    }
+    public string Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+}
+public class HeroLevel
+{
+    private string _name = "";
+    private string _id = "";
+    public string Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+}
